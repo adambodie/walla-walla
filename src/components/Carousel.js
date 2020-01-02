4be883986555ac8769859465691878152bd68292
adambodie <adam.m.bodie@gmail.com>
2020-01-01 17:38:03 -0800

    Added mock data to Carousel component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -22,13 +22,18 @@ const styles = {
 	}
 }
 
-const StyledSlide = withStyles(styles)(Slide);
+const StyledSlide = withStyles(styles)(Slide)
 
 export default class Carousel extends Component {
 	constructor(){
 		super()
 		this.state = { 
-			open: false
+			open: false,
+			data: [
+				{'image': 'palouse', 'title': 'Palouse Falls', 'subtitle': 'Palouse Falls State Park, Washington'},
+				{'image': 'upper-palouse', 'title': 'Upper Palouse Falls', 'subtitle': 'Palouse Falls State Park, Washington'},
+				{'image': 'palouse-river', 'title': 'Palouse River', 'subtitle': 'Palouse Falls State Park, Washington'},
+			]
 		}
 		this.onStart = this.onStart.bind(this)
 		this.onClose = this.onClose.bind(this)
@@ -43,38 +48,26 @@ export default class Carousel extends Component {
 	}
 	
 	render() {
-		const { open } = this.state
+		const { open, data } = this.state
 		return (
 			<div style={{ position: 'relative', width: '100%'}}>
-			<Tooltip title="View Photos" placement="top">
-				<Button style={{padding: '0'}} onClick={() => this.onStart()}>
-					<img src='/img/walla-walla-banner.jpg' style={{width: '100%'}} alt='banner'/>
-				</Button>
-			</Tooltip>
-				<AutoRotatingCarousel
-					autoplay={false}
-					open={open}
-					onClose={() => this.onClose()}
-					onStart={() => this.onStart()}
-					style={{ position: 'absolute' }}
-				>
-					<StyledSlide
-					  media={<img src='/img/palouse.jpg' alt='text'/>}
-					  title='Palouse Falls'
-					  subtitle='Palouse Falls State Park, Washington'
-					/>
-					 <StyledSlide
-					  media={<img src='/img/upper-palouse.jpg' alt='text'/>}
-					  title='Upper Palouse Falls'
-					  subtitle='Palouse Falls State Park, Washington'
-					/>
-					 <StyledSlide
-					  media={<img src='/img/palouse-river.jpg' alt='text'/>}
-					  title='Palouse River'
-					  subtitle='Palouse Falls State Park, Washington'
-					/>
+				<Tooltip title="View Photos" placement="top">
+					<Button style={{padding: '0'}} onClick={() => this.onStart()}>
+						<img src='/img/walla-walla-banner.jpg' style={{width: '100%'}} alt='banner'/>
+					</Button>
+				</Tooltip>
+				<AutoRotatingCarousel open={open} onClose={() => this.onClose()} onStart={() => this.onStart()} >
+					{data.map((x, index) => (
+						<StyledSlide
+							key={index}
+							media={<img src={`/img/${x.image}.jpg`} alt={x.title}/>}
+							title={x.title}
+							subtitle={x.subtitle}
+						/>
+						)
+					)}
 				</AutoRotatingCarousel>
-		</div>
+			</div>
 	)
 }}
 
